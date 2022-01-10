c2f2c808663345c7edcaa182becb2cd0ae94efef
Jérémy <jbarland@stagiaire-humanbooster.com>
2022-01-10 18:04:15 +0100

    add new country form

--- migrations/Version20220110170007.php
+++ migrations/Version20220110170007.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220110145900 extends AbstractMigration
+final class Version20220110170007 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,20 +22,27 @@ final class Version20220110145900 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE account (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(120) NOT NULL, email VARCHAR(255) NOT NULL, nickname VARCHAR(255) DEFAULT NULL, wallet INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, account_id INT DEFAULT NULL, game_id INT NOT NULL, created_at DATETIME NOT NULL, content LONGTEXT NOT NULL, up_votes INT DEFAULT 0 NOT NULL, down_votes INT DEFAULT 0 NOT NULL, INDEX IDX_9474526C9B6B5FBA (account_id), INDEX IDX_9474526CE48FD905 (game_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE game (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, published_at DATETIME NOT NULL, price DOUBLE PRECISION NOT NULL, description LONGTEXT DEFAULT NULL, thumbnail_cover VARCHAR(255) DEFAULT NULL, thumbnail_logo VARCHAR(255) DEFAULT NULL, slug VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE country (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, nationality VARCHAR(255) NOT NULL, flag VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE game (id INT AUTO_INCREMENT NOT NULL, publisher_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, published_at DATETIME NOT NULL, price DOUBLE PRECISION NOT NULL, description LONGTEXT DEFAULT NULL, thumbnail_cover VARCHAR(255) DEFAULT NULL, thumbnail_logo VARCHAR(255) DEFAULT NULL, slug VARCHAR(255) DEFAULT NULL, INDEX IDX_232B318C40C86FCE (publisher_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE game_language (game_id INT NOT NULL, language_id INT NOT NULL, INDEX IDX_7F9F8E93E48FD905 (game_id), INDEX IDX_7F9F8E9382F1BAF4 (language_id), PRIMARY KEY(game_id, language_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE game_genre (game_id INT NOT NULL, genre_id INT NOT NULL, INDEX IDX_B1634A77E48FD905 (game_id), INDEX IDX_B1634A774296D31F (genre_id), PRIMARY KEY(game_id, genre_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE game_country (game_id INT NOT NULL, country_id INT NOT NULL, INDEX IDX_C6B1B649E48FD905 (game_id), INDEX IDX_C6B1B649F92F3E70 (country_id), PRIMARY KEY(game_id, country_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE genre (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE language (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, flag VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE library (id INT AUTO_INCREMENT NOT NULL, game_id INT NOT NULL, account_id INT NOT NULL, installed TINYINT(1) NOT NULL, game_time INT DEFAULT 0 NOT NULL, last_used DATETIME DEFAULT NULL, INDEX IDX_A18098BCE48FD905 (game_id), INDEX IDX_A18098BC9B6B5FBA (account_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE publisher (id INT AUTO_INCREMENT NOT NULL, country_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, director_name VARCHAR(255) NOT NULL, website VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL, INDEX IDX_9CE8D546F92F3E70 (country_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C9B6B5FBA FOREIGN KEY (account_id) REFERENCES account (id)');
         $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CE48FD905 FOREIGN KEY (game_id) REFERENCES game (id)');
+        $this->addSql('ALTER TABLE game ADD CONSTRAINT FK_232B318C40C86FCE FOREIGN KEY (publisher_id) REFERENCES publisher (id)');
         $this->addSql('ALTER TABLE game_language ADD CONSTRAINT FK_7F9F8E93E48FD905 FOREIGN KEY (game_id) REFERENCES game (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE game_language ADD CONSTRAINT FK_7F9F8E9382F1BAF4 FOREIGN KEY (language_id) REFERENCES language (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE game_genre ADD CONSTRAINT FK_B1634A77E48FD905 FOREIGN KEY (game_id) REFERENCES game (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE game_genre ADD CONSTRAINT FK_B1634A774296D31F FOREIGN KEY (genre_id) REFERENCES genre (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE game_country ADD CONSTRAINT FK_C6B1B649E48FD905 FOREIGN KEY (game_id) REFERENCES game (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE game_country ADD CONSTRAINT FK_C6B1B649F92F3E70 FOREIGN KEY (country_id) REFERENCES country (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE library ADD CONSTRAINT FK_A18098BCE48FD905 FOREIGN KEY (game_id) REFERENCES game (id)');
         $this->addSql('ALTER TABLE library ADD CONSTRAINT FK_A18098BC9B6B5FBA FOREIGN KEY (account_id) REFERENCES account (id)');
+        $this->addSql('ALTER TABLE publisher ADD CONSTRAINT FK_9CE8D546F92F3E70 FOREIGN KEY (country_id) REFERENCES country (id)');
     }
 
     public function down(Schema $schema): void
@@ -43,19 +50,26 @@ final class Version20220110145900 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526C9B6B5FBA');
         $this->addSql('ALTER TABLE library DROP FOREIGN KEY FK_A18098BC9B6B5FBA');
+        $this->addSql('ALTER TABLE game_country DROP FOREIGN KEY FK_C6B1B649F92F3E70');
+        $this->addSql('ALTER TABLE publisher DROP FOREIGN KEY FK_9CE8D546F92F3E70');
         $this->addSql('ALTER TABLE comment DROP FOREIGN KEY FK_9474526CE48FD905');
         $this->addSql('ALTER TABLE game_language DROP FOREIGN KEY FK_7F9F8E93E48FD905');
         $this->addSql('ALTER TABLE game_genre DROP FOREIGN KEY FK_B1634A77E48FD905');
+        $this->addSql('ALTER TABLE game_country DROP FOREIGN KEY FK_C6B1B649E48FD905');
         $this->addSql('ALTER TABLE library DROP FOREIGN KEY FK_A18098BCE48FD905');
         $this->addSql('ALTER TABLE game_genre DROP FOREIGN KEY FK_B1634A774296D31F');
         $this->addSql('ALTER TABLE game_language DROP FOREIGN KEY FK_7F9F8E9382F1BAF4');
+        $this->addSql('ALTER TABLE game DROP FOREIGN KEY FK_232B318C40C86FCE');
         $this->addSql('DROP TABLE account');
         $this->addSql('DROP TABLE comment');
+        $this->addSql('DROP TABLE country');
         $this->addSql('DROP TABLE game');
         $this->addSql('DROP TABLE game_language');
         $this->addSql('DROP TABLE game_genre');
+        $this->addSql('DROP TABLE game_country');
         $this->addSql('DROP TABLE genre');
         $this->addSql('DROP TABLE language');
         $this->addSql('DROP TABLE library');
+        $this->addSql('DROP TABLE publisher');
     }
 }
